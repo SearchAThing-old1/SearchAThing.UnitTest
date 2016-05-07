174571257cb504e698bc7d6aa0ecaf00e247e38b
Lorenzo Delana <oss.devel@searchathing.com>
2016-05-08 01:20:16 +0200

    unit tests for vector3d

--- src/Sci.cs
+++ src/Sci.cs
@@ -24,24 +24,44 @@
 #endregion
 
 using SearchAThing.Core;
+using SearchAThing.Sci;
 using System;
 using Xunit;
+using static System.Math;
 
 namespace SearchAThing.UnitTests
 {
 
     public class Sci
-    {     
-
-        #region mu domain [tests]
+    {
+        IModel model = new SampleModel();
 
         [Fact]
-        public void MUDomainTest()
+        public void Vector3DTest()
         {
-            
-        }
+            // length
+            Assert.True(new Vector3D(1, 5.9, 4).Length.EqualsTolLen(7.198, model));
 
-        #endregion
+            // normalized
+            Assert.True(new Vector3D(1, 5.9, 4).Normalized().EqualsTolNormLen(new Vector3D(0.13893, 0.81968, 0.55572), model));
+
+            // distance
+            Assert.True(new Vector3D(1, 5.9, 4).Distance(new Vector3D(3, 4.3, 1.03)).EqualsTolLen(3.922, model));
+
+            // dot product
+            Assert.True(new Vector3D(5, 1, 3).DotProduct(new Vector3D(5, 4, 6)).EqualsTolLen(47, model));
+
+            // cross product
+            Assert.True(new Vector3D(2, 4, 12).CrossProduct(new Vector3D(3, 6, 1)).EqualsTolLen(new Vector3D(-68, 34, 0), model));
+
+            // angle rad
+            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(new Vector3D(1.4325, 2.70248, 0), model)
+                .EqualsTol(0.548, MUCollection.PlaneAngle.rad.Tolerance(model)));
+
+            // angle rad
+            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(new Vector3D(-3.48412, -2.066, 0), model)
+                .EqualsTol(PI, MUCollection.PlaneAngle.rad.Tolerance(model)));
+        }
 
     }
 
