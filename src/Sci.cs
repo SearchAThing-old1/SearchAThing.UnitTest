071bdc61ff6df00b1be197d7c843476ddf63ba74
Lorenzo Delana <oss.devel@searchathing.com>
2016-05-09 23:32:08 +0200

    fixed tolerance checks Changes: tolerance now take first argument in checks to allow argument with default values at end

--- src/Sci.cs
+++ src/Sci.cs
@@ -39,96 +39,98 @@ namespace SearchAThing.UnitTests
         [Fact]
         public void Vector3DTest()
         {
+            var tolLen = model.MUDomain.Length.Value;
+            var tolRad = model.MUDomain.PlaneAngle.Value;
+
             // length
-            Assert.True(new Vector3D(1, 5.9, 4).Length.EqualsTolLen(7.198, model));
+            Assert.True(new Vector3D(1, 5.9, 4).Length.EqualsTol(tolLen, 7.198));
 
             // normalized
-            Assert.True(new Vector3D(1, 5.9, 4).Normalized().EqualsTolNormLen(new Vector3D(0.13893, 0.81968, 0.55572), model));
+            Assert.True(new Vector3D(1, 5.9, 4).Normalized().EqualsTol(Constants.NormalizedLengthTolerance, new Vector3D(0.13893, 0.81968, 0.55572)));
 
             // distance
-            Assert.True(new Vector3D(1, 5.9, 4).Distance(new Vector3D(3, 4.3, 1.03)).EqualsTolLen(3.922, model));
+            Assert.True(new Vector3D(1, 5.9, 4).Distance(new Vector3D(3, 4.3, 1.03)).EqualsTol(tolLen, 3.922));
 
             // dot product
-            Assert.True(new Vector3D(5, 1, 3).DotProduct(new Vector3D(5, 4, 6)).EqualsTolLen(47, model));
+            Assert.True(new Vector3D(5, 1, 3).DotProduct(new Vector3D(5, 4, 6)).EqualsTol(tolLen, 47));
 
             // cross product
-            Assert.True(new Vector3D(2, 4, 12).CrossProduct(new Vector3D(3, 6, 1)).EqualsTolLen(new Vector3D(-68, 34, 0), model));
+            Assert.True(new Vector3D(2, 4, 12).CrossProduct(new Vector3D(3, 6, 1)).EqualsTol(tolLen, new Vector3D(-68, 34, 0)));
 
             // angle rad
-            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(new Vector3D(1.4325, 2.70248, 0), model)
-                .EqualsTol(0.548, MUCollection.PlaneAngle.rad.Tolerance(model)));
+            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(tolLen, new Vector3D(1.4325, 2.70248, 0)).EqualsTol(tolRad, 0.548));
 
             // angle rad
-            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(new Vector3D(-3.48412, -2.066, 0), model)
-                .EqualsTol(PI, MUCollection.PlaneAngle.rad.Tolerance(model)));
+            Assert.True(new Vector3D(3.48412, 2.06577, 0).AngleRad(tolLen, new Vector3D(-3.48412, -2.066, 0)).EqualsTol(tolRad, PI));
 
             // vector projection
             Assert.True(new Vector3D(101.546, 25.186, 1.3).Project(new Vector3D(48.362, 46.564, 5))
-                .EqualsTolLen(new Vector3D(56.491, 14.011, 0.723), model));
+                .EqualsTol(tolLen, new Vector3D(56.491, 14.011, 0.723)));
 
             // vector vers
-            Assert.True(new Vector3D(101.546, 25.186, 1.3).Concordant(new Vector3D(50.773, 12.593, .65)));
-            Assert.False(new Vector3D(101.546, 25.186, 1.3).Concordant(new Vector3D(-50.773, -12.593, .65)));
+            Assert.True(new Vector3D(101.546, 25.186, 1.3).Concordant(tolLen, new Vector3D(50.773, 12.593, .65)));
+            Assert.False(new Vector3D(101.546, 25.186, 1.3).Concordant(tolLen, new Vector3D(-50.773, -12.593, .65)));
 
             // angle toward
-            Assert.True(new Vector3D(120.317, 42.914, 0).AngleToward(new Vector3D(28.549, 63.771, 0), Vector3D.ZAxis, model)
+            Assert.True(new Vector3D(120.317, 42.914, 0).AngleToward(tolLen, new Vector3D(28.549, 63.771, 0), Vector3D.ZAxis)
                 .EqualsTol(0.80726, MUCollection.PlaneAngle.rad.Tolerance(model)));
 
-            Assert.False(new Vector3D(120.317, 42.914, 0).AngleToward(new Vector3D(28.549, 63.771, 0), -Vector3D.ZAxis, model)
+            Assert.False(new Vector3D(120.317, 42.914, 0).AngleToward(tolLen, new Vector3D(28.549, 63.771, 0), -Vector3D.ZAxis)
                 .EqualsTol(0.80726, MUCollection.PlaneAngle.rad.Tolerance(model)));
 
-            Assert.True(new Vector3D(120.317, 42.914, 0).AngleToward(new Vector3D(28.549, 63.771, 0), -Vector3D.ZAxis, model)
+            Assert.True(new Vector3D(120.317, 42.914, 0).AngleToward(tolLen, new Vector3D(28.549, 63.771, 0), -Vector3D.ZAxis)
                 .EqualsTol(2 * PI - 0.80726, MUCollection.PlaneAngle.rad.Tolerance(model)));
 
             // z-axis rotation
-            Assert.True(new Vector3D(109.452, 38.712, 0).RotateAboutZAxis((50.0).ToRad()).EqualsTolLen(new Vector3D(40.699, 108.728, 0), model));
+            Assert.True(new Vector3D(109.452, 38.712, 0).RotateAboutZAxis((50.0).ToRad())
+                .EqualsTol(tolLen, new Vector3D(40.699, 108.728, 0)));
 
             // arbitrary axis rotation
             Assert.True(new Vector3D(747.5675, 259.8335, 0).RotateAboutAxis(new Vector3D(123.151, 353.8977, 25.6), (50.0).ToRad())
-                .EqualsTolLen(new Vector3D(524.3462, 370.9603, -462.4069), model));
+                .EqualsTol(tolLen, new Vector3D(524.3462, 370.9603, -462.4069)));
 
             // rotate relative
             Assert.True(
-                new Vector3D(69.1831, 157.1155, 300).RotateAs(
-                    new Vector3D(443.6913, 107.8843, 0), new Vector3D(342.7154, 239.6307, 0), model)
-                .EqualsTolLen(new Vector3D(7.3989, 171.5134, 300), model));
+                new Vector3D(69.1831, 157.1155, 300).RotateAs(tolLen,
+                    new Vector3D(443.6913, 107.8843, 0), new Vector3D(342.7154, 239.6307, 0))
+                .EqualsTol(tolLen, new Vector3D(7.3989, 171.5134, 300)));
 
             // vector parallel (1-d)
-            Assert.True(new Vector3D(1, 0, 0).IsParallelTo(new Vector3D(-1, 0, 0), model));
-            Assert.True(new Vector3D(0, 1, 0).IsParallelTo(new Vector3D(0, -2, 0), model));
-            Assert.True(new Vector3D(0, 0, 1).IsParallelTo(new Vector3D(0, 0, -3), model));
+            Assert.True(new Vector3D(1, 0, 0).IsParallelTo(tolLen, new Vector3D(-1, 0, 0)));
+            Assert.True(new Vector3D(0, 1, 0).IsParallelTo(tolLen, new Vector3D(0, -2, 0)));
+            Assert.True(new Vector3D(0, 0, 1).IsParallelTo(tolLen, new Vector3D(0, 0, -3)));
 
             // vector parallel (2-d)
-            Assert.True(new Vector3D(1, 1, 0).IsParallelTo(new Vector3D(-2, -2, 0), model));
-            Assert.False(new Vector3D(1, 1, 0).IsParallelTo(new Vector3D(-2, 2, 0), model));
+            Assert.True(new Vector3D(1, 1, 0).IsParallelTo(tolLen, new Vector3D(-2, -2, 0)));
+            Assert.False(new Vector3D(1, 1, 0).IsParallelTo(tolLen, new Vector3D(-2, 2, 0)));
 
-            Assert.True(new Vector3D(1, 0, 1).IsParallelTo(new Vector3D(-2, 0, -2), model));
-            Assert.False(new Vector3D(1, 0, 1).IsParallelTo(new Vector3D(-2, 0, 2), model));
+            Assert.True(new Vector3D(1, 0, 1).IsParallelTo(tolLen, new Vector3D(-2, 0, -2)));
+            Assert.False(new Vector3D(1, 0, 1).IsParallelTo(tolLen, new Vector3D(-2, 0, 2)));
 
-            Assert.True(new Vector3D(0, 1, 1).IsParallelTo(new Vector3D(0, -2, -2), model));
-            Assert.False(new Vector3D(0, 1, 1).IsParallelTo(new Vector3D(0, -2, 2), model));
+            Assert.True(new Vector3D(0, 1, 1).IsParallelTo(tolLen, new Vector3D(0, -2, -2)));
+            Assert.False(new Vector3D(0, 1, 1).IsParallelTo(tolLen, new Vector3D(0, -2, 2)));
 
             // vector parallel (3-d)
-            Assert.True(new Vector3D(1, 1, 1).IsParallelTo(new Vector3D(-2, -2, -2), model));
-            Assert.True(new Vector3D(253.6625, 162.6347, 150).IsParallelTo(new Vector3D(380.4937, 243.952, 225), model));
+            Assert.True(new Vector3D(1, 1, 1).IsParallelTo(tolLen, new Vector3D(-2, -2, -2)));
+            Assert.True(new Vector3D(253.6625, 162.6347, 150).IsParallelTo(tolLen, new Vector3D(380.4937, 243.952, 225)));
 
             // vector parallel ( tolerance checks )
             {
                 // ensure mm tolerance 1e-1
                 var tmpModel = new SampleModel();
 
-                tmpModel.MUDomain.SetLength(new Measure(1e-1, MUCollection.Length.mm));
+                var tmpTolLen = 1e-1;
 
                 // Z component of first vector will be considered zero cause < 1e-1 model tolerance     
-                Assert.True(new Vector3D(10, 0, 0.05).IsParallelTo(new Vector3D(-2, 0, 0), tmpModel));
+                Assert.True(new Vector3D(10, 0, 0.05).IsParallelTo(tmpTolLen, new Vector3D(-2, 0, 0)));
 
                 // Z component of first vector will be considered not-zero cause > 1e-1 model tolerance
-                Assert.False(new Vector3D(10, 0, 0.11).IsParallelTo(new Vector3D(-2, 0, 0), tmpModel));
+                Assert.False(new Vector3D(10, 0, 0.11).IsParallelTo(tmpTolLen, new Vector3D(-2, 0, 0)));
 
                 // X, Z components of first vector force internal usage of normalized tolerance 1e-4
                 // cause the length of the shortest vector here is < 1.5 and may represents a normalized vector
                 // or a result of such type of operation
-                Assert.True(new Vector3D(0.09, 0, 0.09).IsParallelTo(new Vector3D(-20, 0, -20), tmpModel));
+                Assert.True(new Vector3D(0.09, 0, 0.09).IsParallelTo(tmpTolLen, new Vector3D(-20, 0, -20)));
             }
 
         }
